b7d6afa3962dad2e3466f8335d7086b4ae58340b
Maxim Mikhaylov <max@sn3k.dev>
2023-08-02 13:15:18 +0200

    fix code style issues

--- src/main/java/de/infynyty/zuap/insertionHandler/WGZimmerHandler.java
+++ src/main/java/de/infynyty/zuap/insertionHandler/WGZimmerHandler.java
@@ -21,49 +21,52 @@ import java.util.ArrayList;
 import java.util.logging.Level;
 
 public class WGZimmerHandler extends InsertionHandler<WGZimmerInsertion> {
-
+    
+    private final int minPrice = 200;
+    private final int maxPrice = 1500;
+    private final String wgState = "all";
 
     public WGZimmerHandler(@NotNull String logPrefix, @NotNull InsertionAnnouncer announcer, @NotNull HttpClient httpClient) {
         super(logPrefix, announcer, httpClient);
+    }
+    
+    private static WebElement condition(WebDriver driver) {
+        WebElement entry = findElementSafely(driver, By.className("search-result-entry"));
+        if (entry != null) {
+            return entry;
+        }
+
+        final WebElement captchaFail = findElementSafely(driver, By.xpath("//div[@class='text no-link']/h1"));
+        return captchaFail;
     }
 
     //TODO: Make it possible to change search variables
     @Override
     protected String pullUpdatedData() {
-        var options = new FirefoxOptions();
+        FirefoxOptions options = new FirefoxOptions();
         options.addArguments("--headless");
-        var driver = new FirefoxDriver(options);
+        FirefoxDriver driver = new FirefoxDriver(options);
 
         driver.get("https://www.wgzimmer.ch/wgzimmer/search/mate.html");
 
-        WebElement priceMinSelect = driver.findElement(By.name("priceMin"));
-        WebElement priceMaxSelect = driver.findElement(By.name("priceMax"));
-        WebElement wgStateSelect = driver.findElement(By.name("wgState"));
+        final WebElement priceMinSelect = driver.findElement(By.name("priceMin"));
+        final WebElement priceMaxSelect = driver.findElement(By.name("priceMax"));
+        final WebElement wgStateSelect = driver.findElement(By.name("wgState"));
 
-        priceMinSelect.sendKeys("200");
-        priceMaxSelect.sendKeys("1500");
-        wgStateSelect.sendKeys("all");
+        priceMinSelect.sendKeys(String.valueOf(minPrice));
+        priceMaxSelect.sendKeys(String.valueOf(maxPrice));
+        wgStateSelect.sendKeys(wgState);
 
-        WebElement searchButton = driver.findElement(By.xpath("//input[@value='Suchen']"));
+        final WebElement searchButton = driver.findElement(By.xpath("//input[@value='Suchen']"));
         searchButton.click();
 
-        var wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        ExpectedCondition<WebElement> condition = driver1 -> {
-            WebElement entry = findElementSafely(driver1, By.className("search-result-entry"));
-            if (entry != null) {
-                return entry;
-            }
-
-            WebElement captchaFail = findElementSafely(driver1, By.xpath("//div[@class='text no-link']/h1"));
-            return captchaFail;
-        };
-        WebElement element = wait.until(condition);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        final WebElement element = wait.until(WGZimmerHandler::condition);
         if (element.getTagName().equals("h1")) {
             driver.quit();
-            Zuap.log(Level.SEVERE, getHandlerName(), "Captcha failed!");
             throw new RuntimeException("Captcha failed!");
         }
-        var html = driver.getPageSource();
+        String html = driver.getPageSource();
         driver.quit();
 
         return html;
